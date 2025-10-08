7bc427019254e76bdb2cc3a86a468338f4c16f43
hmillot <Huogdu76@gmail.com>
2025-10-08 11:56:04 +0200

    fix: ios no implementation

--- ios/Plugin/DocumentScannerPlugin.swift
+++ ios/Plugin/DocumentScannerPlugin.swift
@@ -9,36 +9,15 @@ import Capacitor
 @objc(DocumentScannerPlugin)
 public class DocumentScannerPlugin: CAPPlugin {
     
-    /** @property  documentScanner the document scanner */
-    let documentScanner = DocScanner()
-    
     /**
-     * start the document scanner and register callbacks
+     * No implementation at the moment
      *
      * @param  call contains JS inputs and lets you return results
      */
     @objc func scanDocument(_ call: CAPPluginCall) {
         // launch the document scanner
-        documentScanner.startScan(
-            bridge?.viewController,
-            successHandler: { (scannedDocumentImages: [String]) in
-                // document scan success
-                call.resolve([
-                    "status": "success",
-                    "scannedImages": scannedDocumentImages
-                ])
-            },
-            errorHandler: { (errorMessage: String) in
-                // document scan error
-                call.reject(errorMessage)
-            },
-            cancelHandler: {
-                // when user cancels document scan
-                call.resolve([
-                    "status": "cancel"
-                ])
-            },
-            responseType: call.getString("responseType")
-        )
+        call.resolve([
+            "status": "cancel"
+        ])
     }
 }
